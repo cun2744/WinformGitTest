45f8df12ea1265e7588f362b5934f028a3b8e465
goddkfvlwl <goddkfvlwl@gmail.com>
2018-11-28 11:19:47 +0900

    버튼 message.Box 기능추가

--- GitTest1/Form1.cs
+++ GitTest1/Form1.cs
@@ -26,5 +26,10 @@ namespace GitTest1
         {
             MessageBox.Show("얌");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("얌얌얌");
+        }
     }
 }
